7fda4086247075b5e530646860eda727a665c204
Arindam Pandey <arindampandey63@gmail.com>
2025-09-29 13:38:46 +0530

    Add files via upload

---
+++ 19.cpp
@@ -0,0 +1,32 @@
+//Constructors.
+#include <iostream>
+using namespace std;
+
+class Complex
+{
+    int a, b;
+
+public:
+    Complex(void);  
+
+    void printNumber()
+    {
+        cout << "Your number is " << a << " + " << b << "i" << endl;
+    }
+};
+
+Complex ::Complex(void) // ----> This is a default constructor as it takes no parameters
+{
+    a = 10;
+    b = 0;
+    // cout<<"Hello world";
+}
+int main()
+{
+    Complex c1, c2, c3;
+    c1.printNumber();
+    c2.printNumber();
+    c3.printNumber();
+
+    return 0;
+}
